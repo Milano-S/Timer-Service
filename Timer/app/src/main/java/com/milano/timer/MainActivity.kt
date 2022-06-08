35fdb86b3a5030cd02ea5abd713eaafab0dd694b
PNNYWISE <101608447+PNNYWISE@users.noreply.github.com>
2022-06-08 03:06:56 +0200

    1rst Commit

---
+++ Timer/app/src/main/java/com/milano/timer/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.milano.timer
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.milano.timer.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var timeStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = com.milano.timer.databinding.ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener{ startStopTimer()}
+        binding.btnReset.setOnClickListener{ resetTimer()}
+
+        serviceIntent = Intent(applicationContext, TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
+    }
+
+
+    private fun resetTimer() {
+        stopTimer()
+        time = 0.0
+        binding.tvTime.text = getTimeStringFromDouble(time)
+    }
+
+    private fun startStopTimer() {
+        if (timeStarted){
+            stopTimer()
+        }else{
+            startTimer()
+        }
+    }
+
+    private fun startTimer() {
+        serviceIntent.putExtra(TimerService.TIME_EXTRA,time)
+        startService(serviceIntent)
+        binding.btnStart.text = "Stop"
+        binding.btnStart.icon = getDrawable(R.drawable.ic_baseline_pause_24)
+        timeStarted = true
+    }
+
+    private fun stopTimer() {
+        stopService(serviceIntent)
+        binding.btnStart.text = "Start"
+        binding.btnStart.icon = getDrawable(R.drawable.ic_baseline_play_arrow_24)
+        timeStarted = false
+    }
+
+    private val  updateTime: BroadcastReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(TimerService.TIME_EXTRA,0.0)
+            binding.tvTime.text = getTimeStringFromDouble(time)
+        }
+    }
+
+    private fun getTimeStringFromDouble(time: Double): String {
+        val resultInt = time.roundToInt()
+        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400%3600/60
+        val seconds = resultInt % 86400 % 3600 % 60
+        
+        return makeTimeString(hours, minutes, seconds)
+    }
+
+    private fun makeTimeString(hours: Int, min: Int, sec: Int): String = String.format("%02d:%02d:%02d", hours,min,sec)
+
+}
